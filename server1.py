8f5fb4ba401c49c85dfb90b55d3d9c01b25ce560
jsellin78 <34406807+jsellin78@users.noreply.github.com>
2023-04-17 02:38:37 +0200

    Create server1.py

---
+++ server1.py
@@ -0,0 +1,67 @@
+#How to run
+# Usage: python3 httpserver.py ip_adress port textfile_path 
+# Use this with inotifywait module and you have a enterprise ready server   
+
+#! /usr/bin/env python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import sys
+import codecs
+import subprocess
+import json
+import html
+
+path = str(sys.argv[3])
+
+
+COLOR = "\033[1;32m"
+RESET_COLOR = "\033[00m"
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+                   #f = open(f'{name}.csv', 'wb') 
+    def do_log(self, method):
+        content_length = self.headers['Content-Length']
+        content_length = 0 if (content_length is None) else int(content_length)
+        post_data = self.rfile.read(content_length)
+        post = post_data.decode('utf-8')
+        post1 = post.strip("\'")
+        with open(str(path),'a', encoding='utf-8') as fd:
+              fd.write(str(post1 + '\n'))
+              print(str(post1))
+              self._set_response()
+
+    def do_GET(self):
+        self.do_log("GET")
+
+    def do_POST(self):
+        self.do_log("POST")
+
+    def do_PUT(self):
+        self.do_log("PUT")
+
+    def do_DELETE(self):
+        self.do_log("DELETE")
+
+ def run(address, port, server_class=HTTPServer, handler_class=S):
+    logging.basicConfig(level=logging.INFO)
+    server_address = (address, port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage:\n" + sys.argv[0] + " [address] [port]")
+        sys.exit(1)
+
+    run(sys.argv[1], int(sys.argv[2]))
+       
