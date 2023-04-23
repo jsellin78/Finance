81f3615f7b298278bc24fa94e876b1c5c99e9112
jsellin78 <34406807+jsellin78@users.noreply.github.com>
2023-04-23 19:23:36 +0200

    Update time.sh

--- time.sh
+++ time.sh
@@ -7,10 +7,6 @@ YELLOW="$(tput setaf 3)"
 NOCOLOR="$(tput sgr0)"
 
 
-
-#echo "day and hour time"
-#read sentiment
-
 year=$(date +%Y)
 month=$(date +%m)
 #month=$(date -d "1 month ago" +'%m')
@@ -18,50 +14,39 @@ minute="00"
 ninteen="72" 
 sixteen="60"
 noweekend="64"
+ted="19:00"
 
 
 args=("$@")
-#echo $# arguments passed
-#echo "$year"-"$month"-${args[0]} ${args[1]}":$minute" 
-                  #day       #hour 
 
-# HOW THE FORMAT LOOKS 
+# format outlook
 #startdate=$(date -d "1500 2023-04-17 +3 hours" +"%Y-%m-%d %H:%M")
 
-#When Pattern found 
+#When Pattern found. startpoint  
 startdate=$(date -d "${args[1]}$minute $year"-"$month"-"${args[0]}" +"%Y-%m-%d %H:%M")
 
 form=$(date -d "${args[1]}$minute $year"-"$month"-"${args[0]}" +"%Y-%m-%d %H:%M" | tr '-' ' ' | sed 's/.\{5\}$//')
-
-#IF THIS VALUE IS GREATER THAN FRIDAY THEN WE KNOW PATTERN CREATED OUTSIDE OF WEEKEND.   
+ 
 endate=$(date -d "${args[1]}$minute $year"-"$month"-"${args[0]} +$sixteen hour" +"%Y-%m-%d %H:%M") 
 
-echo "form is" $form
-#friday=`python3 ./upcomingfriday.py $form` #prints the exact date of comming friday 
+friday123=`python3 ./friday123.py $form` #prints the exact time of upcomming friday close 
 
-friday123=`python3 ./friday123.py $form` #prints the exact date of comming friday 
-#fridayy=`python3 ./friday.py "$form" | tr ' ' '-' | '19:00'` #prints the exact date of comming friday 
 VAL1=$(date +%s -d"$endate")
 VAL2=$(date +%s -d"$friday123")
 
-#echo "endate Pattern was created at" $endate
-ted="19:00"
-
-if [[ "$VAL1" > "$VAL2" ]]; then #if value is greater than friday close. Then we know that the pattern was created next week  
+if [[ "$VAL1" > "$VAL2" ]]; then #if value is greater than friday close. Then we know that the pattern was created the next week  
     echo "Pattern is greater than friday" #
     hours_inbetween=`python3 ./busyday.py "$startdate" "$friday123 $ted" | sed "s/\..*//"` 
     sunday=`python3 ./sunday.py $form`
     val=$(expr \( $sixteen - $hours_inbetween \))
     left_over=$(date -d "$(LC_TIME=C date -d "$sunday")+$val hour" +"%Y-%m-%d %H:%M" | tr '-' ' ' | tr ':' ' ')  
     echo "final 16x pattern after weekend is" ${YELLOW}$left_over${YELLOW} $NOCOLOR
-    #B=$(date -d "$(LC_TIME=C date -d "$left_over")+$val hour" +"%Y-%m-%d %H:%M") 
     echo "Pattern was created at"${NOCOLOR} ${YELLOW}$startdate${YELLOW} $NOCOLOR
     echo "Sunday is" $sunday
-    echo "Friday is" $friday123 # "friday is " $friday
+    echo "Friday is" $friday123 
     echo "hours from startdate to friday close" $hours_inbetween
-    echo $val "Missing hours from sunday to next week"
+    echo $val "hours from sunday to next week"
     python3 ./timer.py $left_over ${args[2]}
-    /root/newstart/candle.sh first '/root/tmp/${args[2]}4hour.txt'
     
 else
     echo "startdate" $startdate
@@ -70,10 +55,8 @@ else
     echo "No weekend" $left 
 fi
     
-#When Pattern found 
 startdate1=$(date -d "${args[1]}$minute $year"-"$month"-"${args[0]}" +"%Y-%m-%d %H:%M")
-form=$(date -d "${args[1]}$minute $year"-"$month"-"${args[0]}" +"%Y-%m-%d %H:%M" | tr '-' ' ' | sed 's/.\{5\}$//')
-#IF THIS VALUE IS GREATER THAN FRIDAY THEN WE KNOW PATTERN CREATED OUTSIDE OF WEEKEND.   
+form=$(date -d "${args[1]}$minute $year"-"$month"-"${args[0]}" +"%Y-%m-%d %H:%M" | tr '-' ' ' | sed 's/.\{5\}$//')  
 endate1=$(date -d "${args[1]}$minute $year"-"$month"-"${args[0]} +$ninteen hour" +"%Y-%m-%d %H:%M") 
 
 VAL3=$(date +%s -d"$endate1")
@@ -88,4 +71,4 @@ if [[ "$VAL3" > "$VAL4" ]]; then
     echo "final 19x pattern after weekend is" ${YELLOW}$_over${YELLOW} $NOCOLOR
     echo "Pattern was created at"${NOCOLOR} ${YELLOW}$startdate1${YELLOW} $NOCOLOR
     echo "Sunday is" $sunday
-    echo "Friday is" $friday123 # "friday is " $friday
+    echo "Friday is" $friday123 
