93aa604f20cba1db65659fda84aa7999d896d8c9
jsellin78 <34406807+jsellin78@users.noreply.github.com>
2023-05-02 12:50:02 +0200

    Update priceAlert.py

--- priceAlert.py
+++ priceAlert.py
@@ -11,7 +11,7 @@ session = requests.Session()
 chat_id="" #Telegramchatid
 token="" #Token
 
-#python3 ./priceAlert.py --file=/root/tmp/currency.txt --currency=usdsek --target=1.20000 --timeout=86400 --workers=2 --type=above --interval=180 & 
+#python3 ./priceAlert.py --file=/root/tmp/currency.txt --currency=usdsek --target=1.20000 --timeout=86400 --workers=2 --type=below --interval=180 & 
 
 #jsonstring from file {"Time":"2023-05-01 01:13","us30":34093.0,"gbpjpy":171.198,"gbpusd":1.25628,"cadjpy":100.59,"usdjpy":136.266,"nas100":13238.4,"gbpnzd":2.03217,"usdcad":1.35451,"eurusd":1.10153,"eurchf":0.98478,"euraud":1.66636,"eurgbp":0.87671,"eurjpy":150.154,"eurcad":1.492,"nzdusd":0.61805,"audnzd":1.06924,"audusd":0.66097,"xauusd":1990.94,"btcusd":29405.5,"ger40":15940.1,"jpn225":29071.9,"cn50":13209.5,"xngusd":0.0,"gbpcad":1.70159,"gbpaud":1.90034,"eurnzd":1.78187,"audcad":0.89524,"nzdcad":0.83708,"uk100":7867.9,"xpdusd":1499.2,"xptusd":1074.35,"xaujpy":271444.0,"sugar":27.088,"coffee":186.77,"cotton":80.467,"cocoa":2936.6,"wheat":619.8,"soybeans":1446.3,"lumber":463.5,"corn":6.0683,"vix":19.69,"rghrice":17.57,"XAGUSD":25.081,"xaueur":1807.24,"xauaud":3011.56,"xageur":22.758,"XAUGBP":1584.05,"xagaud":37.927,"Copper":3.8791,"usdcnh":6.92465,"gertec30":3262.4,"NatGas":2.235,"OJ":275.76,"SpotCrude":76.624,"SpotBrent":80.372,"Gasoline":2.5412,"usdchf":0.89381,"nzdchf":0.5524,"chfjpy":152.369,"usdx":101.213,"eurx":1043.2,"jpyx":813.9,"XAUCHF":1779.26,"AUDJPY":90.093}
 
