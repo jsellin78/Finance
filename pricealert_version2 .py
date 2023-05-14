5a11d56781fc1371fc2b511323a36561a5979395
jsellin78 <34406807+jsellin78@users.noreply.github.com>
2023-05-14 19:26:37 +0200

    pricealert_version2 .py

--- pricealert_version2 .py
+++ pricealert_version2 .py
@@ -1,3 +1,4 @@
+
 import time 
 import ujson  
 from os import SEEK_CUR, SEEK_END
@@ -7,17 +8,17 @@ import argparse
 import requests
 import math
 import logging
-import time
+import threading 
 
-chat_id="" #Enter Telegramchat_id
-TOKEN="" #Token
+chat_id = ""  # Enter Telegram chat_id
+TOKEN = ""  # Token
 session = requests.Session()
 
 logging.basicConfig(filename='price_alert.log', level=logging.DEBUG)
 
-#Instead of grabbing price from a file we go directly to api_endpoint.py 
+
 def get_price_info():
-    url = "http://192.168.1.220:8013/posts"
+    url = "http://192.168.1.220:8014/posts"
     response = session.get(url)
     data = response.json()
     time_str = data['time']
@@ -25,40 +26,60 @@ def get_price_info():
     del prices['time']
     return time_str, prices
 
+
+def send_telegram_alert(currency, current_price, target_price, time_str, alert_type, pattern_date):
+    if alert_type == 'below':
+        message = f"Price Alert for {currency}! Current price: {current_price:.5f} is below target price: {target_price:.5f} as of {time_str} created at {pattern_date}"
+    elif alert_type == 'above':
+        message = f"Price Alert for {currency}! Current price: {current_price:.5f} is above target price: {target_price:.5f} as of {time_str} created at {pattern_date}"
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    response = session.get(url)
+    if response.status_code == 200:
+        print(message)
+        logging.info(message)
+    else:
+        logging.error(f'Error: Failed to send message. Status code: {response.status_code}')
+
+
 def price_alert(args):
     currency, target_price, max_run_time, alert_type, wait_time, pattern_date = args
-    start_time = time.time()
-    while time.time() - start_time < max_run_time:
+    start_time = time.monotonic()
+    next_check_time = start_time + wait_time
+    timer = None  # Initialize timer to None
+    while time.monotonic() - start_time < max_run_time:
         time_str, prices = get_price_info()
-        tolerance = 1e-4
         if prices.get(currency):
-            if alert_type == 'below' and math.isclose(prices[currency], target_price, rel_tol=1e-4):
+            if alert_type == 'below' and prices[currency] < target_price:
                 # trigger an alert (e.g. send a Telegram message)
-                url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text='Price Alert for {currency}! Current price: {prices[currency]:.5f} is below target price: {target_price:.5f} as of {time_str} created at {pattern_date}'"
-                response = session.get(url)
-                if response.status_code == 200:
-                    print(f'Price Alert for {currency}! Current price: {prices[currency]:.5f} is below target price: {target_price:.5f} as of {time_str} created {pattern_date}')
-                    alert_triggered = True
-                    #print("Target price reached. Stopping the script...")
-                    break
-                else:
-                    logging.error(f'Error: Failed to send message. Status code: {response.status_code}')
-            elif alert_type == 'above' and math.isclose(prices[currency], target_price, rel_tol=1e-4):
+                send_telegram_alert(currency, prices[currency], target_price, time_str, alert_type, pattern_date)
+                break
+            elif alert_type == 'above' and prices[currency] > target_price:
                 # trigger an alert (e.g. send a Telegram message)
-                if response.status_code == 200:
-                    logging.info(f'Price Alert for {currency}! Current price: {prices[currency]:.5f} is above target price: {target_price:.5f} as of {time_str}')
-                    logging.info("Target price reached. Stopping the script...")
-                    alert_triggered = True
-                    break
-                else:
-                    logging.error(f'Error: Failed to send message. Status code: {response.status_code}')
-        time.sleep(wait_time)
+                send_telegram_alert(currency, prices[currency], target_price, time_str, alert_type, pattern_date)
+                break
+        current_time = time.monotonic()
+        if current_time >= next_check_time:
+           next_check_time += wait_time
+           if timer:
+               timer.cancel()
+           timer = threading.Timer(0, lambda: None)
+           timer.start()
+        else:
+            time_left = next_check_time - current_time
+            if not timer:
+                timer = threading.Timer(time_left, lambda: None)
+                timer.start()
+            else:
+                timer.cancel()
+                timer = threading.Timer(time_left, lambda: None)
+                timer.start()
+            timer.join()
 
 
 def main():
     parser = argparse.ArgumentParser(description='Run price alert.')
     parser.add_argument('--currency', dest='currency', help='Currency to monitor', required=True)
-    parser.add_argument('--target', dest='target_price', type=float, help='Target price for the currency', required=True)
+    parser.add_argument('--target', dest='target_price', type=int, help='Target price for the currency', required=True)
     parser.add_argument('--timeout', dest='max_run_time', type=int, help='Maximum run time in seconds', required=True)
     parser.add_argument('--workers', dest='workers', type=int, help='Number of worker processes', required=True)
     parser.add_argument('--type', dest='alert_type', choices=['above', 'below'], help='Type of price alert (above/below target price)', required=True)
@@ -66,6 +87,8 @@ def main():
     parser.add_argument('--patterndate', dest='pattern_date', type=str, help='Pattern date for the currency', required=True)
     args = parser.parse_args()
 
+    print(f"Currency: {args.currency}")
+    print(f"Import time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
     # Create a list of arguments for each worker process to minimize cpu usage 
     worker_args = [(args.currency, args.target_price, args.max_run_time, args.alert_type, args.wait_time, args.pattern_date) for i in range(args.workers)]
 
@@ -76,3 +99,5 @@ def main():
 
 if __name__ == '__main__':
     main()        
+
+            
