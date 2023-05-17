9a30c2e421d7a5436e6403f1b915640da7874dff
jsellin78 <34406807+jsellin78@users.noreply.github.com>
2023-05-17 16:31:02 +0200

    Cell.java

---
+++ Cell.java
@@ -0,0 +1,20 @@
+
+public class Cell {
+    private boolean alive;
+
+    public Cell(boolean alive) {
+        this.alive = alive;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public void toggleState() {
+        alive = !alive;
+    }
+}
