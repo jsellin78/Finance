9806fd7f4df312d686a1abb53a02ec8e98c4b1a0
jsellin78 <34406807+jsellin78@users.noreply.github.com>
2023-05-18 20:08:46 +0200

    Telegram_input.py

--- Telegram_input.py
+++ Telegram_input.py
@@ -3,9 +3,11 @@ from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import html
 from decimal import Decimal 
+import shlex
 
 # Parse telegram input and do some action based on the input. 
 
+# Define the function to handle the 'n' command
 def launch_script_n(update, context):
     # Get the arguments passed by the user
     message_text = update.message.text.lower()
@@ -13,35 +15,30 @@ def launch_script_n(update, context):
 
     if len(args) != 3:
         # Send an error message to the user indicating the incorrect argument format
-        update.message.reply_text("Incorrect argument format for 'n' command. Please provide 3 arguments. Day hour currency ")
-        return 
+        update.message.reply_text("Incorrect argument format for 'n' command. Please provide 3 arguments. n Day hour currency ")
 
     command = ['tmux', 'send-keys', '-t', 'ifvalues', '/root/newstart/newtest1.sh'] + [' '] + list(' '.join(args)) + [' '] + ['&'] + ['Enter']
+ # Execute the another script with arguments using subprocess
     subprocess.run(command)
 
-def Price_alert(update, context, number, currency, below, args):
+def Price_alert(update, context, number, currency, sentiment, args, description):
     # Get the arguments passed by the user
     message_text = update.message.text.lower()
     args = message_text.split()[1:]  # Remove the first element 'n' from the message text
 
-    if len(args) != 2:
-        # Send an error message to the user indicating the incorrect argument format
-        update.message.reply_text("Incorrect argument format for 'price alert command'. Please provide 3 arguments. price_target currency below/above ")
-        return 
-
-
-    command = ['tmux', 'send-keys', '-t', 'ifvalues', 'python3 ', '/root/ifvalue/currency/pricealert.py'] + \
+    command = ['tmux', 'send-keys', '-t', 'ifvalues', 'python3 ', '/root/price/pricealert.py'] + \
             [' '] + ['--currency='] + [currency] + \
             [' '] + ['--target='] + [str(number)] + \
             [' '] + ['--timeout='] + ['200000'] + \
-            [' '] + ['--type='] + [below] + \
+            [' '] + ['--type='] + [sentiment] + \ 
             [' '] + ['--interval='] + ['2'] + \
             [' '] + ['--workers='] + ['2'] + \
             [' '] + ['--patterndate='] + ['2023-01-24'] + \
-            [' '] + ['&', 'Enter']
-    
-    subprocess.run(command)
+            [' '] + ['--description='] + [shlex.quote(str(description))] + \
+            [' '] + ['&', 'Enter'] 
 
+
+    subprocess.run(command)
 
 def launch_script_p(update, context):
     # Execute your script for command 'p' using subprocess and capture the output
@@ -57,16 +54,15 @@ def launch_script_p(update, context):
 
     
 def help_p(update, context):
-    update.message.reply_text("For 4 hour you need to provide DAY HOUR currency \ for 15 min you need to provide DAY HOUR MINUTE CURRENCY")
-
+    update.message.reply_text("For 4 hour you need to provide n DAY HOUR currency \ for 15 min you need to provide y DAY HOUR MINUTE CURRENCY")
 
 def launch_script_y(update, context):
-    # Get the arguments passed by the user
+    # Get the arguments 
     message_text = update.message.text.lower()
     args = message_text.split()[1:]  # Remove the first element 'y' from the message text
 
-    if len(args) != 4
-        update.message.reply_text("Incorrect argument format for 'y' command. Please provide 4 arguments. DAY HOUR MINUTE CURRENCY")
+    if len(args) != 4:
+        update.message.reply_text("Incorrect argument format for 'y' command. Please provide 4 arguments. y DAY HOUR MINUTE CURRENCY")
         return
 
     command = ['tmux', 'send-keys', '-t', 'ifvalues', '/root/newstart/15min/15min2.sh'] + [' '] + list(' '.join(args)) + [' '] + ['&'] + ['Enter']
@@ -78,9 +74,11 @@ def handle_message(update, context):
     message_text = update.message.text.lower()
 
     # Check if the message text is 'p'
-    if message_text == 'p' 
+    if message_text == 'p':
+        # If user type p then run func p 
         launch_script_p(update, context)
-    elif message_text.startswith('y ');
+    elif message_text.startswith('y '):
+        
         launch_script_y(update, context)
     elif message_text.startswith('n '):
         launch_script_n(update, context)
@@ -95,9 +93,13 @@ def handle_message(update, context):
              number_str = Decimal(parts[0])
              number = str(number_str)
              currency = parts[1]
-             below = parts[2]
+             sentiment = parts[2]
              args = parts[3:5]
-             Price_alert(update, context, number, currency, below, args)
+             if len(parts) >= 4:
+                 description = ' '.join(parts[3:])
+             else:
+                 description = None
+             Price_alert(update, context, number, currency, sentiment, args, description)
          else:
              update.message.reply_text("Incorrect argument format. Please provide a number followed by two arguments.")    
     else:
@@ -107,6 +109,7 @@ def handle_message(update, context):
 updater = Updater(token='TOKEN')
 dispatcher = updater.dispatcher
 
+# Register the command handler for 'p' command
 dispatcher.add_handler(CommandHandler('p', launch_script_p))
 
 dispatcher.add_handler(CommandHandler('number', Price_alert))
@@ -114,7 +117,7 @@ dispatcher.add_handler(CommandHandler('number', Price_alert))
 dispatcher.add_handler(CommandHandler('h', help_p, pass_args=True))
 
 dispatcher.add_handler(CommandHandler('n', launch_script_n))
-
+# Register the message handler
 dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
 
 # Start the bot
